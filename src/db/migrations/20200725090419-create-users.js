478a38ca5a02646b31994e10077c5cef541f8375
shivam makwan <shivammakwan124@gail.com>
2021-03-06 19:45:30 +0530

    default date stamp added for migrations

--- src/db/migrations/20200725090419-create-users.js
+++ src/db/migrations/20200725090419-create-users.js
@@ -39,10 +39,12 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW()"),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW() ON UPDATE NOW()"),
       },
     });
   },
